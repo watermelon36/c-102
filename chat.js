a8176156c59b97a3f8ae49c946621fe54725afbe
watermelon36 <106351185+watermelon36@users.noreply.github.com>
2023-01-17 17:59:29 -0500

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+//ADD YOUR FIREBASE LINKS HERE
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+    window.location = "chat_room.html";
+    
+}
+
+
+
